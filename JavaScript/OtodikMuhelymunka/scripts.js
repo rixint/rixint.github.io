2034702557bd1e5646230a4aba77611b3fbbdfee
rixint <90652539+rixint@users.noreply.github.com>
2021-09-19 14:15:21 +0200

    Started Fifth műhelymunka

---
+++ JavaScript/OtodikMuhelymunka/scripts.js
@@ -0,0 +1,44 @@
+let age = 2021-1978
+console.log (age) + "years"
+let ww2 = 1945-1939
+console.log (ww2)
+let tesla = 759.49 + "years"
+console.log (tesla)
+let secondsofDay = 60*60*24 + "s"
+console.log (secondsofDay)
+let hungarians = 10000000/7500000000 + "%"
+console.log (hungarians)
+let bigger = 9.597000-78871 + "m^2"
+console.log (bigger)
+let participants = 654/7
+console.log (participants)
+
+let trueOrFalse = 5 === 5;
+trueOrFalse = 100 < 50;
+trueOrFalse = 'hello' !== 'world';
+trueOrFalse = 1 >= 2;
+trueOrFalse = 'blue'.length === 'green'.length;
+trueOrFalse = 3 < 4 && (5 > 6 || 7 < 8);
+
+let names = ['Béla', 'Hedvig', 'Géza', 'János']
+trueOrFalse = (names[2].length) > 5
+names.push ('Júlia', 'Márk')
+console.log (names)
+
+names[0] = "Bea"
+console.log (names)
+
+console.log (trueOrFalse = (names.length) > 5)
+
+let post = {
+    colors: ['green', 'grey', 'blue', 'yellow', 'red'],
+    author: "GézaBéla",
+    content: "Colours of my day",
+}
+
+
+post.hasMyColors = trueOrFalse = post.colors.length > 3
+
+console.log (post)
+console.log (post.colors)
+
